d7fb7b1a09383e8055d3429ec2e516f7f58df881
anna.bekhter@example.com <anna.bekhter@inbox.ru>
2022-06-24 18:26:00 +0300

    Добавили метод, который выводит на печать массив с нашими данными

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -22,3 +22,11 @@ class HomeWork
                 }
             }
         }
+         void PrintArray(string[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            Console.WriteLine();
+        }
